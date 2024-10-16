8267361dc9478e7c31fd09b909c1e00fd4c137fc
Kah Kit <kahkitzheng@gmail.com>
2024-10-16 10:01:49 +0200

    Switch tabs

--- src/assets/components/App/App.tsx
+++ src/assets/components/App/App.tsx
@@ -1,11 +1,35 @@
-import HighlightQuestion from "../HighlightQuestion/HighlightQuestion";
+import { useState } from "react";
+import TextHighlight from "../TextHighlight/TextHighlight";
 import "./App.scss";
 
 function App() {
+  const [isEditing, setIsEditing] = useState(true);
+  const [data, setData] = useState("");
+
   return (
-    <main>
-      <HighlightQuestion />
-    </main>
+    <>
+      <header>
+        <div>
+          <input
+            type="radio"
+            id="is-not-editing"
+            checked={isEditing}
+            onChange={() => setIsEditing(true)}
+          />
+          <label htmlFor="is-not-editing">Content developer</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="is-editing"
+            checked={!isEditing}
+            onChange={() => setIsEditing(false)}
+          />
+          <label htmlFor="is-editing">Teacher & students</label>
+        </div>
+      </header>
+      <main>{isEditing ? <TextHighlight /> : <p>test</p>}</main>
+    </>
   );
 }
 
